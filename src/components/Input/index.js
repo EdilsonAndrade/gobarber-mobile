27ec2b0249af88b72bdb8d5456524b09ec4c6d13
Edilson Andrade <edilson.a.andrade@gmail.com>
2020-01-21 08:28:03 -0300

    signin page

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-
+import React, {forwardRef} from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import PropTypes from 'prop-types';
 import {Container, TInput} from './styles';
 
-export default function Input({style, ...rest}) {
+function Input({style, icon, ...rest}, ref) {
   return (
     <Container style={style}>
-      <TInput {...rest} />
+      {icon && <Icon name={icon} size={20} color="rgba(255,255,255,0.6)" />}
+      <TInput {...rest} ref={ref} />
     </Container>
   );
 }
+Input.propTypes = {
+  icon: PropTypes.string,
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+};
+
+Input.defaultProps = {
+  style: {},
+  icon: null,
+};
+
+export default forwardRef(Input);
--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import {Container, Text} from './styles';
+import {Image} from 'react-native';
+import logo from '~/assets/logo.png';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmiButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 import Background from '~/components/Background';
 
 export default function Signin() {
   return (
     <Background>
       <Container>
-        <Text>Login page</Text>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu e-mail"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite seu e-mail"
+          />
+        </Form>
+        <SubmiButton onPress={() => {}}>Entrar</SubmiButton>
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
       </Container>
     </Background>
   );
